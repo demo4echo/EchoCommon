48426ecb0878adbbac5ee6c0ed0fbbfcc4216234
root <admin@example.com>
2020-01-14 12:31:39 +0200

    Update

--- runPipelineFlow.sh
+++ runPipelineFlow.sh
@@ -2,16 +2,15 @@
 
 export MARK_OFF_CLUSTER_INVOCATION_ENV_VAR="dc"
 
-#cp ./.gradle/gradle.properties $USER/.gradle/gradle.properties
-
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPT_PATH=$(dirname "$SCRIPT")
-echo $SCRIPT_PATH
 
-#pushd ..
-#./gradlew $# AIO
-#popd
+pushd $SCRIPT_PATH
+cp ./.gradle/gradle.properties $USER/.gradle/gradle.properties
+popd
+
+./gradlew $# AIO
 
 rm $USER/.gradle/gradle.properties
