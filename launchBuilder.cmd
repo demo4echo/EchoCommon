93d52f314b01361a54f8c3b1ea9202e37e5000d6
root <admin@example.com>
2020-01-14 12:42:09 +0200

    Update

--- launchBuilder.cmd
+++ launchBuilder.cmd
@@ -1,4 +1,4 @@
-@echo off
+@ECHO OFF
 
 REM Minikube formatr (needs to add some manipulation on the path:
 REM If the path to mounted (local) is => C:\Users\tmeltse.COMVERSE\OneDrive - Mavenir Ltd\GitClones\GitHub\eclipse
@@ -6,4 +6,4 @@ REM Then the manipulated path would be => /c/Users/tmeltse.COMVERSE/OneDrive - M
 REM So the updated command would be => docker run -it --rm -v "/c/Users/tmeltse.COMVERSE/OneDrive - Mavenir Ltd/GitClones/GitHub/eclipse/echofe":$HOME/repo alpine ash
 
 REM Docker Desktop format (no manipulation is needed), but the C drive needs to be shared once through the Docker Desktop GUI!
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v "%userprofile%\.gradle":/root/.gradle -v "%userprofile%\.helm":/root/.helm -v "%cd%":/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' demo4echo/alpine_openjdk8_k8scdk:latest ash
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v "%USERPROFILE%\.gradle":/root/.gradle -v "%USERPROFILE%\.helm":/root/.helm -v "%CD%":/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' demo4echo/alpine_openjdk8_k8scdk:latest ash
