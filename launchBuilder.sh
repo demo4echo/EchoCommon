f6b6cb6a63bed946d7479da5b4e9e7bfea6d73b8
root <admin@example.com>
2020-01-15 17:54:53 +0200

    Update

--- launchBuilder.sh
+++ launchBuilder.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.gradle:/root/.gradle -v $HOME/.helm:/root/.helm -v `pwd`:/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' demo4echo/alpine_openjdk8_k8scdk:latest ash
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.gradle:/root/.gradle -v $HOME/.helm:/root/.helm -v "$PWD/..":/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' -e COMMON_SUB_MODULE_NAME_ENV_VAR="${PWD##*/}" demo4echo/alpine_openjdk8_k8scdk:latest ash
