0e79f17cc15a2102ec25843d1d626b8ba3f129fe
Tiran Meltser <tiran.meltser@efrat.com>
2020-02-23 20:43:32 +0200

    update

--- launchBuilder.cmd
+++ launchBuilder.cmd
@@ -7,4 +7,4 @@ REM So the updated command would be => docker run -it --rm -v "/c/Users/tmeltse.
 
 REM Docker Desktop format (no manipulation is needed), but the C drive needs to be shared once through the Docker Desktop GUI!
 FOR %%I IN (.) DO SET CURRENT_FOLDER_NAME=%%~nxI
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v "%USERPROFILE%\.gradle":/root/.gradle -v "%USERPROFILE%\.helm":/root/.helm -v "%CD%\..":/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' -e COMMON_SUB_MODULE_NAME_ENV_VAR="%CURRENT_FOLDER_NAME%" demo4echo/alpine_openjdk8_k8scdk:latest ash
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v "%USERPROFILE%\.gradle":/root/.gradle -v "%USERPROFILE%\.helm":/root/.helm -v "%CD%\..":/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' -e COMMON_SUB_MODULE_NAME_ENV_VAR="%CURRENT_FOLDER_NAME%" demo4echo/alpine_openjdk8_k8scdk:latest sh
