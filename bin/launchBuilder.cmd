8a5d2a358df7c6d0cd11ece44e1aed252c57724c
Tiran Meltser <tiran.meltser@efrat.com>
2020-03-07 00:18:54 +0200

    update

--- bin/launchBuilder.cmd
+++ bin/launchBuilder.cmd
@@ -8,13 +8,18 @@ REM So the updated command would be => docker run -it --rm -v "/c/Users/tmeltse.
 REM Docker Desktop format (no manipulation is needed), but the C drive needs to be shared once through the Docker Desktop GUI!
 FOR %%I IN (.) DO SET CURRENT_FOLDER_NAME=%%~nxI
 FOR %%I IN (..) DO SET COMMON_SUB_MODULE_FOLDER_NAME=%%~nxI
+
+PUSHD ..\..
+SET REPO_FOLDER_PATH=%CD%
+POPD
+
 docker run ^
 	-it ^
 	--rm ^
 	-v /var/run/docker.sock:/var/run/docker.sock ^
 	-v "%USERPROFILE%\.gradle":/root/.gradle ^
 	-v "%USERPROFILE%\.helm":/root/.helm ^
-	-v "%CD%\..\..":/root/repo ^
+	-v "%REPO_FOLDER_PATH%":/root/repo ^
 	-w /root/repo/%COMMON_SUB_MODULE_FOLDER_NAME%/%CURRENT_FOLDER_NAME% ^
 	-e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' ^
 	-e COMMON_SUB_MODULE_NAME_ENV_VAR="%COMMON_SUB_MODULE_FOLDER_NAME%" ^
--- bin/launchBuilder.sh
+++ bin/launchBuilder.sh
@@ -6,13 +6,17 @@ cd ..
 COMMON_SUB_MODULE_FOLDER_NAME=${PWD##*/}
 cd -
 
+cd ../..
+REPO_FOLDER_PATH=$PWD
+cd -
+
 docker run \
 		-it \
 		--rm \
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-v $HOME/.gradle:/root/.gradle \
 		-v $HOME/.helm:/root/.helm \
-		-v "$PWD/../..":/root/repo \
+		-v "$REPO_FOLDER_PATH":/root/repo \
 		-w /root/repo/$COMMON_SUB_MODULE_FOLDER_NAME/$CURRENT_FOLDER_NAME \
 		-e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' \
 		-e COMMON_SUB_MODULE_NAME_ENV_VAR="$COMMON_SUB_MODULE_FOLDER_NAME" \
