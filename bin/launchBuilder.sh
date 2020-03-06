8c9ca7bfe6a6fdd44823e90fa2f406a4bd8fa490
Tiran Meltser <tiran.meltser@efrat.com>
2020-03-06 23:56:27 +0200

    update

--- bin/launchBuilder.sh
+++ bin/launchBuilder.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env sh
 
-cd ..
 CURRENT_FOLDER_NAME=${PWD##*/}
+
+cd ..
+COMMON_SUB_MODULE_FOLDER_NAME=${PWD##*/}
 cd -
 
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.gradle:/root/.gradle -v $HOME/.helm:/root/.helm -v "$PWD/../..":/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' -e COMMON_SUB_MODULE_NAME_ENV_VAR="$CURRENT_FOLDER_NAME" demo4echo/alpine_openjdk8_k8scdk:latest sh
+docker run \
+		-it \
+		--rm \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		-v $HOME/.gradle:/root/.gradle \
+		-v $HOME/.helm:/root/.helm \
+		-v "$PWD/../..":/root/repo \
+		-w /root/repo/$COMMON_SUB_MODULE_FOLDER_NAME/$CURRENT_FOLDER_NAME \
+		-e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' \
+		-e COMMON_SUB_MODULE_NAME_ENV_VAR="$COMMON_SUB_MODULE_FOLDER_NAME" \
+		demo4echo/alpine_openjdk8_k8scdk:latest \
+		sh
