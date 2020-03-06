5011dd87ff8abac8d9dc41eaa95e45c3744febd8
Tiran Meltser <tiran.meltser@efrat.com>
2020-03-06 23:57:33 +0200

    update

--- bin/launchBuilder.cmd
+++ bin/launchBuilder.cmd
@@ -19,4 +19,4 @@ docker run ^
 	-e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' ^
 	-e COMMON_SUB_MODULE_NAME_ENV_VAR="%COMMON_SUB_MODULE_FOLDER_NAME%" ^
 	demo4echo/alpine_openjdk8_k8scdk:latest ^
-	sh
+	ash
