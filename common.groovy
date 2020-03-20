9d9b9eacfec9bbe328d26fb125518612297799b9
Tiran Meltser <tiran.meltser@efrat.com>
2020-03-20 14:46:06 +0200

    update

--- common.groovy
+++ common.groovy
@@ -21,17 +21,16 @@ def manifestVersion(boolean concatenateBranchNameAsSuffix = true) {
 	def currentBranchName = obtainCurrentBranchName()
 	def versionSuffix = concatenateBranchNameAsSuffix == true ? "-${currentBranchName}" : ""
 	def currentVersionName = project.version.toString() // must be done this way since reckon makes project.version non serializable
-	def dockerSafeTagName = currentVersionName.replace(CONST_UNSAFE_CHARACTER_FOR_DOCKER_TAG_NAME,CONST_SAFE_CHARACTER_REPLACER_FOR_DOCKER_TAG_NAME)
+//	def dockerSafeTagName = currentVersionName.replace(CONST_UNSAFE_CHARACTER_FOR_DOCKER_TAG_NAME,CONST_SAFE_CHARACTER_REPLACER_FOR_DOCKER_TAG_NAME)
 	def manifestedVersion = "${insignificant_version_notation}${versionSuffix}"
 
 	// Check if we are to work with a designated version (tag) - in which case use it
 	if (project.hasProperty(CONST_DESIGNATED_TAG_NAME_PROJECT_PROPERTY_NAME) == true && project.ext[CONST_DESIGNATED_TAG_NAME_PROJECT_PROPERTY_NAME].trim().isBlank() == false) {
 		manifestedVersion = "${project.ext[CONST_DESIGNATED_TAG_NAME_PROJECT_PROPERTY_NAME]}${versionSuffix}"
 	}
-
-	// Check if version is significant - in which case use reckon based version (tag) (don't use else-if since we need to work on the most updated 'manifestedVersion')
-	if (isSignificantVersion(manifestedVersion) == true) {
-		manifestedVersion = "${dockerSafeTagName}${versionSuffix}"
+	// Otherwise, check if version is significant - in which case use reckon based version (tag)
+	else if (isSignificantVersion(currentVersionName) == true) {
+		manifestedVersion = "${currentVersionName}${versionSuffix}"
 	}
 
 	return manifestedVersion
@@ -51,7 +50,8 @@ def manifestNamespace() {
 
 // Checks if the applicable version marks a significant version or not
 def isSignificantVersion(String versionName) {
-	def isSignificantVersion = (versionName.contains(CONST_SAFE_CHARACTER_REPLACER_FOR_DOCKER_TAG_NAME) == false && versionName.contains(CONST_UNSAFE_CHARACTER_FOR_DOCKER_TAG_NAME) == false)
+//	def isSignificantVersion = (versionName.contains(CONST_SAFE_CHARACTER_REPLACER_FOR_DOCKER_TAG_NAME) == false && versionName.contains(CONST_UNSAFE_CHARACTER_FOR_DOCKER_TAG_NAME) == false)
+	def isSignificantVersion = (versionName.contains(versionName.contains(CONST_UNSAFE_CHARACTER_FOR_DOCKER_TAG_NAME) == false)
 
 	return isSignificantVersion
 }
@@ -76,7 +76,7 @@ ext {
 	CONST_DESIGNATED_TAG_MESSAGE_PROJECT_PROPERTY_NAME='demo4echo.designatedTagMessage'
 	CONST_BRANCH_SPECIFIC_CONFIGURATION_FILE_NAME='branchSpecificConfig.properties'
 	CONST_UNSAFE_CHARACTER_FOR_DOCKER_TAG_NAME='+'
-	CONST_SAFE_CHARACTER_REPLACER_FOR_DOCKER_TAG_NAME='_'
+//	CONST_SAFE_CHARACTER_REPLACER_FOR_DOCKER_TAG_NAME='_'
 
 	// Functions
 	obtainCurrentBranchName = this.&obtainCurrentBranchName
